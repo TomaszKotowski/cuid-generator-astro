945b83ed0de43be0e9cbed8fea5872db07af5265
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-09-04 08:08:03 +0200

    init commit

---
+++ src/components/CopyToClipboardButton.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let id = "";
+
+  const handleCopyToClipboard = async () => {
+    return await navigator.clipboard.writeText(id);
+  };
+</script>
+
+<button on:click={handleCopyToClipboard} value={id}>Copy</button>
