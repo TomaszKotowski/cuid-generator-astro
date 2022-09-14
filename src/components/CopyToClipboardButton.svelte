b4670b4c2a78d7946a594dbb5a1dcc47f8bcb7f6
Tomasz Kotowski <t.kotowski.2@gmail.com>
2022-09-14 13:43:00 +0200

    add tooltip after copy

--- src/components/CopyToClipboardButton.svelte
+++ src/components/CopyToClipboardButton.svelte
@@ -1,9 +1,21 @@
 <script>
   export let id = "";
+  let tooltipVisible = false;
+
+  const handleTooltipVisibility = () => {
+    tooltipVisible = true;
+    setTimeout(() => (tooltipVisible = false), 2000);
+  };
 
   const handleCopyToClipboard = async () => {
+    handleTooltipVisibility();
     return await navigator.clipboard.writeText(id);
   };
 </script>
 
-<button type="button" on:click={handleCopyToClipboard} value={id}>Copy</button>
+<button
+  data-tooltip={tooltipVisible ? "Copied!" : null}
+  type="button"
+  on:click={handleCopyToClipboard}
+  value={id}>Copy</button
+>
